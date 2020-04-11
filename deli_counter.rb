4a194b5abf5b9fd37c519beeaf4be64a819a5f3e
Andrew Bourgeois <andrew@olympicmedia.com>
2020-04-11 22:26:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ def line(andre_deli)
     end
     binding.pry
     line_status = "The line is currently: "
-    line_status + numbered_line
+    puts line_status + numbered_line.join
   end
 end
 
